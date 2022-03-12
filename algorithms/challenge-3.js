b0009636d57b63b75a5c9a9659f7e1f8892c5ca3
positivelizzy <55453747+positivelizzy@users.noreply.github.com>
2022-03-12 13:07:20 +0100

    Update challenge-3.js

--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,3 +1,12 @@
-function binaryReversal(value) {}
-
+function binaryReversal(value) {
+int rev = o
+}
+While( n > 0 )
+rev << = 1
+if n & 1 == 1, then
+reversal = 1
+else n >> 1
+{
+return reeversal
+}
 module.exports = binaryReversal
