a13b8e4bb85fd8917d4cdee1565b4d8cfa16cce0
positivelizzy <55453747+positivelizzy@users.noreply.github.com>
2022-03-12 11:51:02 +0100

    Update challenge-1.js

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,3 +1,29 @@
-function romanToDecimal(roman) {}
-
+function romanToDecimal(roman) {
+    var result = 0;
+  
+     for (i = 0; i < str.length; i++) 
+}
+         var s1 = value(str.charAt(i));
+  
+         if (i + 1 < str.length) 
+         {
+             var s2 = value(str.charAt(i + 1));
+ 
+             if (s1 >= s2) 
+             {
+                 result = result + s1;
+             } 
+             else 
+             {
+                 result = result + s2 - s1;
+                 i++;
+             }
+         } 
+         else  
+         {
+             result = result + s1;
+         }
+     }
+     return result;
+}
 module.exports = romanToDecimal
