07146a493aa012e7c27004b1ab724cf4b7f8ec4f
positivelizzy <55453747+positivelizzy@users.noreply.github.com>
2022-03-12 12:08:18 +0100

    Update challenge-2.js

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,3 +1,13 @@
-function transpose(arr) {}
+function transpose(arr) {
+       let arr=[];
+        for(let i=0;i<matrix.length;i++){
+            arr.push([])
+            for(let j=0;j<matrix.length;j++){
+                arr[i].push(matrix[j][i])
+            }
+        }
+        return arr
+    }
+}
 
 module.exports = transpose
